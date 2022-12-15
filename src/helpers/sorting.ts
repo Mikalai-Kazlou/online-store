83d1313bfb3ae4099f20ba65d2532f1de65c93b0
rockmonolit <rockmonolit@mail.ru>
2022-12-15 18:07:27 +0300

    feat: add basic filter logic

---
+++ src/helpers/sorting.ts
@@ -0,0 +1,1 @@
+import goodsData from '../goods';
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ import './style.scss';
 import './script.ts';
 import './modal.ts';
 import './goods.ts';
+import './helpers/get-values';
+import './helpers/filters';
