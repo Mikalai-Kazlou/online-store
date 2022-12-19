7f4e4d6cce9a03be9b6b4b4c8c2b7653e9432988
rockmonolit <rockmonolit@mail.ru>
2022-12-19 23:22:00 +0300

    feat: expand stock buttons class

--- src/components/StockButtons/StockButtons.ts
+++ src/components/StockButtons/StockButtons.ts
@@ -1,7 +1,6 @@
 import Cart from '../Cart/Cart';
 import Goods from '../Goods/Goods';
 import Header from '../Header/Header';
-import { elementNullCheck } from '../../types/type-checks';
 
 export default class StockButtons {
   private parent: HTMLElement;
@@ -17,71 +16,95 @@ export default class StockButtons {
   }
 
   draw(): void {
+    const stockSelector = document.createElement('div');
+    stockSelector.classList.add('main-text');
+    stockSelector.classList.add('stock-selector');
+
     const itemPrice = document.createElement('p');
     itemPrice.classList.add('info-price');
     itemPrice.classList.add('main-text');
-    itemPrice.innerHTML = `$${this.goods.price}`
+    itemPrice.innerHTML = `Price: $${this.goods.price}`;
 
     const amountButtons = document.createElement('div');
     amountButtons.classList.add('amount-buttons');
     amountButtons.classList.add('main-text');
-    amountButtons.addEventListener('click', this.setStock);
 
     const minusButton = document.createElement('button');
     minusButton.classList.add('amount-button');
     minusButton.classList.add('main-text');
+    minusButton.classList.add('button-decrement');
     minusButton.innerHTML = '-';
 
     const selectedAmount = document.createElement('p');
     selectedAmount.classList.add('selected-stock');
     selectedAmount.classList.add('main-text');
-    selectedAmount.innerHTML = '1';
+    selectedAmount.innerHTML = `${this.getCurrentAmount(this.goods)}`;
 
     const plusButton = document.createElement('button');
     plusButton.classList.add('amount-button');
     plusButton.classList.add('main-text');
+    plusButton.classList.add('button-increment');
     plusButton.innerHTML = '+';
 
-    this.parent.appendChild(itemPrice);
-    this.parent.appendChild(amountButtons);
+    plusButton.addEventListener('click', () => this.incrementStock(this.goods, selectedAmount, this.cart));
+    minusButton.addEventListener('click', () => this.decrementStock(this.goods, selectedAmount, this.cart));
+
+    this.parent.appendChild(stockSelector);
+    stockSelector.appendChild(itemPrice);
+    stockSelector.appendChild(amountButtons);
     amountButtons.appendChild(minusButton);
     amountButtons.appendChild(selectedAmount);
     amountButtons.appendChild(plusButton);
-  }
-
-  setStock(event: Event): void {
-    console.log(this.goods)
-    const selectedAmount = elementNullCheck(document, '.selected-stock');
-    let currentStock = +selectedAmount.innerHTML || 1;
-    const maxStock = this.goods.stock;
-    const infoPrice = elementNullCheck(document, '.info-price');
-    if (event.target) {
-      const target = event.target as HTMLButtonElement;
-      const clickedOption = target.closest('button');
-      if (clickedOption?.innerHTML === '+' && currentStock < maxStock) {
-        currentStock++;
-        selectedAmount.innerHTML = `${currentStock}`;
-        this.setPrice(infoPrice, this.goods.price, currentStock);
-        if (this.cart.has(this.goods)) {
-          this.cart.drop(this.goods);
-          for (let i = 0; i < currentStock; i++) {
-            this.cart.add(this.goods);
-          }
-        }
-      } else if (clickedOption?.innerHTML === '-' && currentStock > 1) {
-        currentStock--;
-        selectedAmount.innerHTML = `${currentStock}`;
-        this.setPrice(infoPrice, this.goods.price, currentStock);
-        if (this.cart.has(this.goods)) {
-          this.cart.drop(this.goods);
-        }
-      }
-    }
     this.header.refreshHeader();
   }
 
-  setPrice(parent: Element, price: number, selectedStock: number) {
+  incrementStock(goods: Goods, stock: HTMLParagraphElement, cart: Cart) {
+    const priceContainer = document.querySelector('.info-price') as HTMLParagraphElement;
+    const amount = this.getCurrentAmount(goods);
+    const maxStock = goods.stock;
+    let currentStock = +stock.innerHTML;
+    if (amount < maxStock && cart.has(goods)) {
+      cart.add(goods);
+      stock.innerHTML = `${currentStock + 1}`;
+      this.header.refreshHeader();
+    } else if (!cart.has(goods)) {
+      stock.innerHTML = `${currentStock + 1}`;
+      this.header.refreshHeader();
+    }
+    this.setPrice(priceContainer, goods.price, +stock.innerHTML);
+  }
+
+  decrementStock(goods: Goods, stock: HTMLParagraphElement, cart: Cart): void {
+    const priceContainer = document.querySelector('.info-price') as HTMLParagraphElement;
+    const amount = this.getCurrentAmount(goods);
+    let currentStock = +stock.innerHTML;
+    if (amount > 1 && cart.has(goods) && currentStock > 1) {
+      cart.drop(goods);
+      stock.innerHTML = `${currentStock - 1}`;
+      for (let index = 0; index < currentStock - 1; index++) {
+        cart.add(goods);
+      }
+      this.header.refreshHeader();
+    } else if (!cart.has(goods) && currentStock > 1) {
+      stock.innerHTML = `${currentStock - 1}`;
+      this.header.refreshHeader();
+    }
+    this.setPrice(priceContainer, goods.price, +stock.innerHTML);
+  }
+
+  getCurrentAmount(goods: Goods): number {
+    const goodsArray = this.cart.getEntries().map((item) => item.id);
+    let result = 1;
+    if (goodsArray.filter((item) => item === goods.id).length > 0) {
+      result = goodsArray.filter((item) => item === goods.id).length;
+    } else {
+      result = 1;
+    }
+    return result;
+  }
+
+  setPrice(parent: Element, price: number, selectedStock: number): void {
     const finalPrice = price * selectedStock;
-    parent.innerHTML = `$${finalPrice}`;
+    parent.innerHTML = `Price: $${finalPrice}`;
   }
 }
