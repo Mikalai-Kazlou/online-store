48930d0658a1aeef6ab1d999b791ff205b6e2587
Mikalai Kazlou <mikalai_kazlou@epam.com>
2023-01-08 16:29:16 +0300

    docs: change console note

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,8 @@ import './components/GoodsData';
 import './components/Header';
 
 console.log(
-  'All functional and technical requirements of the task are met:\n - Cross-check: 300/300\n - Mentor check: 200/200\n',
-  '\nNote: Sometimes images take a long time to load due to limitations on the test resource server. Thanks for your understanding.'
+  'All functional and technical requirements of the task are met:\n - Cross-check: 300/300\n - Mentor check: 200/200\n\n',
+  'Note: Sometimes images take a long time to load due to limitations on the test resource server (error 429).\n',
+  '      Thanks for your understanding.\n',
+  ' '
 );
